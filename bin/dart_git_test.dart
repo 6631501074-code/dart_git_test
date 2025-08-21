da7271971db274b54875c22a4c92cd158a502010
Doid888 <6631501074@lamduan.mfu.ac.th>
2025-08-21 16:18:53 +0700

    initial commit test

---
+++ bin/dart_git_test.dart
@@ -0,0 +1,8 @@
+
+void sum(int one , int two) {
+  // compute the sum and print result
+}
+
+void main() {
+  sum(1, 2);
+}
---
+++ test/dart_git_test_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_git_test/dart_git_test.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
